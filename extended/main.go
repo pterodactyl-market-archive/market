f90bee2a4d69a88c08f0c7db11ebbccdbab32261
theMackabu <theMackabu@gmail.com>
2022-07-24 19:11:21 +0000

    (BASE): Pocketbase init

--- extended/main.go
+++ extended/main.go
@@ -13,7 +13,6 @@ func main() {
 	app := pocketbase.New()
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		// serves static files from the provided public dir (if exists)
 		subFs := echo.MustSubFS(e.Router.Filesystem, "pb_public")
 		e.Router.GET("/*", apis.StaticDirectoryHandler(subFs, false))
 
