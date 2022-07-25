f3d1da4309cb9a751a1f7bb70f70d4a2cb35c1a9
Sajjaad Farzad <theMackabu@gmail.com>
2022-07-25 16:48:52 +0430

    remove from base

--- apis/base.go
+++ apis/base.go
@@ -15,7 +15,6 @@ import (
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/tools/rest"
 	"github.com/pocketbase/pocketbase/ui"
-	"github.com/pocketbase/pocketbase/market"
 	"github.com/spf13/cast"
 )
 
@@ -74,7 +73,6 @@ func InitApi(app core.App) (*echo.Echo, error) {
 	}
 
 	// ui embed routes
-	bindStaticMarketUI(app, e)
 	bindStaticAdminUI(app, e)
 
 	// default routes
@@ -151,29 +149,6 @@ func bindStaticAdminUI(app core.App, e *echo.Echo) error {
 
 	return nil
 }
-
-// bindStaticMarketUI registers the endpoints that serves the static market UI.
-func bindStaticMarketUI(app core.App, e *echo.Echo) error {
-	// serves /market/dist/index.html file
-	// (explicit route is used to avoid conflicts with `RemoveTrailingSlash` middleware)
-	e.FileFS(
-		"/",
-		"index.html",
-		market.DistIndexHTML,
-		middleware.Gzip(),
-	)
-
-	// serves static files from the /market/dist directory
-	// (similar to echo.StaticFS but with gzip middleware enabled)
-	e.GET(
-		"/*",
-		StaticDirectoryHandler(market.DistDirFS, false),
-		middleware.Gzip(),
-	)
-
-	return nil
-}
-
 
 const totalAdminsCacheKey = "totalAdmins"
 
